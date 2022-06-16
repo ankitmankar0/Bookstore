492fcfb2218b0a19d4b864d7914a56769b9530f0
ankitmankar0 <ankitmankar012@gmail.com>
2022-06-16 23:24:50 +0530

    Get all Book

--- BooksDataBase.sql
+++ BooksDataBase.sql
@@ -57,3 +57,88 @@ BEGIN
 Delete BookTable 
 where BookId = @BookId;
 End;
+
+--procedure to updatebook
+create procedure spUpdateBook
+(
+@BookId int,
+@BookName varchar(255),
+@AuthorName varchar(255),
+@TotalRating int,
+@RatingCount int,
+@OriginalPrice Decimal,
+@DiscountPrice Decimal,
+@BookDetails varchar(255),
+@BookImage varchar(255),
+@BookQuantity int
+)
+as
+BEGIN
+Update BookTable set BookName = @BookName, 
+AuthorName = @AuthorName,
+TotalRating = @TotalRating,
+RatingCount = @RatingCount,
+OriginalPrice= @OriginalPrice,
+DiscountPrice = @DiscountPrice,
+BookDetails = @BookDetails,
+BookImage =@BookImage,
+BookQuantity = @BookQuantity
+where BookId = @BookId;
+End;
+
+
+-- create procedure to get all book 
+create procedure spGetAllBook
+as
+BEGIN
+	select * from BookTable;
+End;
+
+---------------------------------------------------------------------------------End Of Book -----------------------------------------------------------------------
+
+create Table CartTable
+(
+CartId int primary key identity(1,1),
+BooksQty int,
+UserId int Foreign Key References Users(UserId),
+BookId int Foreign Key References BookTable(BookId)
+);
+
+select * from CartTable;
+
+DROP TABLE CartTable;
+
+---create procedure to addcart
+create Procedure spAddCart
+(
+@BooksQty int,
+@UserId int,
+@BookId int
+)
+As
+Begin
+Insert into CartTable (BooksQty,UserId,BookId) 
+values (@BooksQty,@UserId,@BookId);
+End;
+
+
+DROP PROCEDURE spAddCart;
+GO
+
+---Create procedure to deletecart
+create procedure spDeleteCart
+@CartId int
+As
+Begin 
+Delete CartTable where CartId = @CartId
+End
+
+---create procedure to UpdateCart
+create procedure spUpdateCart
+@BooksQty int,
+@CartId int
+As
+Begin
+update CartTable set BooksQty = @BooksQty
+where CartId = @CartId
+End
--- RepositoryLayer/Interface/IBookRL.cs
+++ RepositoryLayer/Interface/IBookRL.cs
@@ -8,9 +8,9 @@ namespace RepositoryLayer.Interface
     public interface IBookRL
     {
         public BookModel AddBook(BookModel book);
-        //public BookModel GetBookByBookId(int BookId);
-        //public bool DeleteBook(int BookId);
-        //bool UpdateBook(int BookId, BookModel updateBook);
-        //public List<BookModel> GetAllBooks();
+        public BookModel GetBookByBookId(int BookId);
+        public bool DeleteBook(int BookId);
+        bool UpdateBook(int BookId, BookModel updateBook);
+        public List<BookModel> GetAllBooks();
     }
 }
